65de28e329ec2a91dea33228d397353e9ee7ec28
Elek Viktor <elekviktor2003@gmail.com>
2022-12-19 18:04:52 +0100

    2. Feladat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+class Versenytav {
+    private String rajtSzam;
+
+    public String getTav() {
+        switch (rajtSzam.charAt(0)) {
+            case 'M':
+                return "Mini";
+            case 'R':
+                return "Rövid";
+            case 'K':
+                return "Közép";
+            case 'H':
+                return "Hosszú";
+            case 'E':
+            default:
+                return "Pedelec";
+        }
+    }
+
+    public Versenytav(String rajtszam) {
+        rajtSzam = rajtszam;
+    }
+}
 class BukkMaraton2019 {
     public static void main(String[] args) {
-        
+
     }
 }
