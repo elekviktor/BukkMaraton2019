4546c46a88d3dcd8a8962c841b2c2fb27928d5e3
Elek Viktor <elekviktor2003@gmail.com>
2022-12-19 18:07:08 +0100

    3. Feladat

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+//2. Feladat
 class Versenytav {
     private String rajtSzam;
 
@@ -21,8 +26,61 @@ class Versenytav {
         rajtSzam = rajtszam;
     }
 }
+
+// 3. Feladat
+class Versenyzo {
+    private String rajtszam;
+    private String kategoria;
+    private Versenytav versenytav;
+    private String nev;
+    private String egyesulet;
+    private String ido;
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public String getRajtszam() {
+        return rajtszam;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public String getEgyesulet() {
+        return egyesulet;
+    }
+
+    public String getIdo() {
+        return ido;
+    }
+
+    Versenyzo(String sor) {
+        String[] darabolt = sor.split(";");
+        rajtszam = darabolt[0];
+        versenytav = new Versenytav(darabolt[0]);
+        kategoria = darabolt[1];
+        nev = darabolt[2];
+        egyesulet = darabolt[3];
+        ido = darabolt[4];
+    }
+}
+
 class BukkMaraton2019 {
     public static void main(String[] args) {
-
+        // 3. Feladat
+        List<Versenyzo> versenyzoList = new ArrayList<>();
+        File inputFile = new File("bukkm2019.txt");
+        try (Scanner scanner = new Scanner(inputFile)) {
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String aktualisSor = scanner.nextLine();
+                versenyzoList.add(new Versenyzo(aktualisSor));
+            }
+        } catch (FileNotFoundException exception) {
+            System.err.print("Fájl nem található!");
+            return;
+        }
     }
 }
